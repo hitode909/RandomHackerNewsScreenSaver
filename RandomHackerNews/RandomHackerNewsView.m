3d840f3d5c13e00139dcde810f42e0f8a2ea2ded
hitode909 <hitode909@gmail.com>
2020-05-05 18:06:05 +0900

    Initial Commit

---
+++ RandomHackerNews/RandomHackerNewsView.m
@@ -0,0 +1,52 @@
+//
+//  RandomHackerNewsView.m
+//  RandomHackerNews
+//
+//  Created by hitode909 on 2020/05/05.
+//  Copyright © 2020 hitode909. All rights reserved.
+//
+
+#import "RandomHackerNewsView.h"
+
+@implementation RandomHackerNewsView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
